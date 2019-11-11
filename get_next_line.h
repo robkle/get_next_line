731f48ace4ff2d406e53727e966fa886bf2366aa
Robert Klein <rklein@c3r3p6.hive.fi>
2019-11-11 12:13:17 +0200

    second round

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rklein <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/04 11:15:42 by rklein            #+#    #+#             */
+/*   Updated: 2019/11/11 11:50:49 by rklein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 8
+# define FD_MAX 1024
+
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+int get_next_line(const int fd, char **line);
+
+#endif
